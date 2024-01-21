e9eacd3e6e99f0fe01af2ef247f77ce19914828a
Louis Geist <louisjjgeist@gmail.com>
2024-01-21 17:21:26 +0100

    fusion graph module working

--- GAE_tools.py
+++ GAE_tools.py
@@ -1,4 +1,5 @@
 import torch
+import torch_geometric
 from torch_geometric.nn import global_mean_pool
 from torch.nn.functional import cosine_similarity # pdist
 
@@ -27,5 +28,72 @@ def batched_similarity_matrix(graph_batch):
     
     return similarity_matrix
 
+
+def fusion(graph_batch, k, beta): 
+    """
+    returns the similarity matrix and the adjacency matrix of a graph (combining initial graph and knn graph)
+    see Lin et al. (2023)
+
+    Input :
+     - graph_batch contains the following attributes : x, edge_index, batch
+    Ouput : 
+     - fusion_graph : torch_geometric.Data object representing A_F
+                    with attributes x, edge_index, edge_attr, batch
+    """
+
+    x = graph_batch.x
+    edge_index = graph_batch.edge_index
+    batch = graph_batch.batch
+    n = x.shape[0]
+
+    A = torch_geometric.utils.to_dense_adj(edge_index,batch = batch)
+
+
+    similarity_matrix = batched_similarity_matrix(graph_batch)
+
+    # Step : knn graph
+    _,nearest_neighbors = torch.topk(similarity_matrix, dim=1, largest=True, k=k+1)
+    mask = (nearest_neighbors.unsqueeze(1) == nearest_neighbors.unsqueeze(0)).any(dim=-1).to(torch.int)
+    mask = mask - torch.eye(n, dtype=torch.int)
+    A_K = mask * similarity_matrix
+
+
+    # passage de A_K (N,N) à (32,n_max,n_max)
+    batch_size = batch.max().item() + 1
+    n_max = A.shape[1]
+    A_K_reshape = torch.zeros(batch_size, n_max, n_max)
+
+    for batch_num in range(batch_size):
+        batch_ind = torch.where(batch == batch_num*torch.ones(n))[0]
+
+        begin = batch_ind.min().item()
+        end = batch_ind.max().item()
+
+        current_n_node = end + 1 - begin
+
+        A_K_reshape[batch_num,:current_n_node,:current_n_node] = A_K[begin:end+1 , begin:end+1]
+
+
+    # Step :  fusion
+    
+
+    A_F = beta * A + (1-beta)*A_K_reshape
+    print("A_F :",A_F.shape)
+
+    edge_indices = []
+    edge_attrs = []
+
+    for batch_idx in range(batch_size):
+        edge_index = A_F[batch_idx].nonzero().t()
+
+        edge_attr = A_F[batch_idx][edge_index[0], edge_index[1]].view(-1, 1)
+
+        edge_indices.append(edge_index)
+        edge_attrs.append(edge_attr)
+
+    final_edge_index = torch.cat(edge_indices, dim=1)
+    final_edge_attr = torch.cat(edge_attrs, dim=0)
+
+    return similarity_matrix, (final_edge_index, final_edge_attr)
 
 
